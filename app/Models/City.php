82e6ae77a22840c2e3f577e8c6df1961a2958829
pedrofreitasmt <pedrohenrique_of@hotmail.com>
2025-08-19 23:30:53 -0400

    feat: adicionar modelo City com relacionamentos

---
+++ app/Models/City.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    protected $fillable = [
+        'name',
+        'uf',
+        'ibge',
+        'state_id'
+    ];
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+}
