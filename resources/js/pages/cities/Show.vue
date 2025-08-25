3a35a97546a0bb2985f1b72bc1046b3ed1aef6db
pedrofreitasmt <pedrohenrique_of@hotmail.com>
2025-08-25 07:35:15 -0400

    feat: criar componente Show para exibir detalhes das cidades

---
+++ resources/js/pages/cities/Show.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import Pagination from '@/components/Pagination.vue';
+</script>
+
+<template>
+
+<h1>chejfsef</h1>
+</template>
