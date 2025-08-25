ddfc69bc333c2ba266647e1453b93bd01ac5b131
pedrofreitasmt <pedrohenrique_of@hotmail.com>
2025-08-25 07:35:21 -0400

    feat: atualizar rota inicial para renderizar página de login

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('auth/Login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resource('cities', CityController::class)->only('index', 'show');
 });
 
 require __DIR__.'/settings.php';
-require __DIR__.'/auth.php'; 
+require __DIR__.'/auth.php';
